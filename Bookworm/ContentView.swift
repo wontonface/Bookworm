0aad122cff091fac1716c70d52655951d5d320e2
Nicole Garcia <nicole@nicoleiliana.com>
2025-01-31 16:43:52 -0800

    Added edit, swipe to delete functionality

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -10,7 +10,9 @@ import SwiftData
 
 struct ContentView: View {
     @Environment(\.modelContext) var modelContext
-    @Query var books: [Book]
+    @Query(sort: [SortDescriptor(\Book.title),
+                  SortDescriptor(\Book.author)
+                 ]) var books: [Book]
     
     @State private var showingAddScreen = false
     
@@ -33,12 +35,16 @@ struct ContentView: View {
                         }
                     }
                 }
+                .onDelete(perform: deleteBooks)
             }
             .navigationTitle("Bookworm")
             .navigationDestination(for: Book.self) { book in
                 DetailView(book: book)
             }
             .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    EditButton()
+                }
                 ToolbarItem(placement: .topBarTrailing) {
                     Button("Add book", systemImage: "plus") {
                         showingAddScreen.toggle()
@@ -48,6 +54,13 @@ struct ContentView: View {
             .sheet(isPresented: $showingAddScreen) {
                 AddBookView()
             }
+        }
+    }
+    
+    func deleteBooks(at offsets: IndexSet) {
+        for offset in offsets {
+            let book = books[offset]
+            modelContext.delete(book)
         }
     }
 }
